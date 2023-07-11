23dd04daee49e48850b1dfa64d66b7f006914c22
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-11 11:42:09 -0700

    Add test proj code and change path

--- CoinCombos/Models/CoinCombos.cs
+++ CoinCombos/Models/CoinCombos.cs
@@ -4,6 +4,30 @@ namespace CoinCombos
 {
     public class CoinCombo
     {
-        // add class functionality
+        // public int Quarters { get; set; }
+        // public int Dimes { get; set; }
+        // public int Nickels { get; set; }
+        // public int Pennies { get; set; }
     }
+
+    // public class CalculateCoins
+    // {
+    //     // public static CoinCombo CalculateSmallestCoins(int cents)
+    //     // {
+    //     //     CoinCombo coinCombo = new CoinCombo();
+
+    //     //     coinCombo.Quarters = cents / 25;
+    //     //     cents = cents % 25;
+
+    //     //     coinCombo.Dimes = cents / 10;
+    //     //     cents = cents % 10;
+
+    //     //     coinCombo.Nickels = cents / 5;
+    //     //     cents = cents % 5;
+
+    //     //     coinCombo.Pennies = cents;
+
+    //     //     return coinCombo;
+    //     // }
+    // }
 }
